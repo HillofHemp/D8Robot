cabd09c29b5b073d20d0a189e8c8301f58dcf5b4
wdarvin <wesdarvin@gmail.com>
2015-03-10 18:08:08 -0400

    First try at getAngle function
    Eric really needs to add comments because he is a good programer and
    that’s what they do

---
+++ getAngle.h
@@ -0,0 +1,27 @@
+#import <cmath>
+#DEFINE RADTODEG 57.2957795
+double getAngle(double fx, double fy)	{
+	double sx, sy, distance, angle, dx, dy, radicand;
+	
+	
+	sx = RPS.X();
+	sy = RPS.Y();
+	dx = fx-sx;
+	dy = fy-sy;
+	
+	radicand = pow(dx,2) + pow(dy, 2);
+	distance = sqrt(radicand);
+	
+	if (dy >= 0)	{
+		angle = acos(dx/distance) * RADTODEG;
+	} else if (dy < 0  && dx < 0)	{
+		angle = (360 - acos(dx/distance))*RADTODEG;
+	} else if (dy < 0 && dx >= 0)	{
+		angle = -acos(dx/distance)*RADTODEG;
+	}
+	
+	return angle;
+}
+	
+	
+	
